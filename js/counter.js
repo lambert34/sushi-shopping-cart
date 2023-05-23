01a57229e0eb0b03eccc35bb409c005e97492624
lambert34 <aleksandrlambert@gmail.com>
2023-05-23 15:07:05 +0700

    добавляет кнопки + -
    сначала скрипт находит кнопки по дата-селекторам
    далее кнопка при нажатии включает положительный и отрицательный инкременты

---
+++ js/counter.js
@@ -0,0 +1,18 @@
+const btnMinus = document.querySelector('[data-action="minus"]');
+
+const btnPlus = document.querySelector('[data-action="plus"]');
+
+const counter = document.querySelector('[data-counter]');
+
+console.log(counter);
+
+btnMinus.addEventListener('click', function() {
+    console.log('Minus clk');
+    counter.innerText = --counter.innerText;
+});
+
+btnPlus.addEventListener('click', function() {
+    console.log('Plus clk');
+    counter.innerText = ++counter.innerText;
+});
+
